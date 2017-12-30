117f148e83eccea3993c341aa34ccda7a5801bb9
Jim Cooper <jim@cooperplanet.com>
2017-12-30 15:33:46 -0700

    Fix errors on login form

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -9,6 +9,8 @@ import { Router } from '@angular/router'
   `]
 })
 export class LoginComponent {
+  username
+  password
   constructor(private authService:AuthService, private router:Router) {
 
   }
